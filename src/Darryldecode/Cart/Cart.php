92ac9f5010591a2b3740edb6e40b249a5e721e4a
PiotrZapolski <48650230+PiotrZapolski@users.noreply.github.com>
2022-10-19 19:38:45 +0200

    gotowa paczka

--- src/Darryldecode/Cart/Cart.php
+++ src/Darryldecode/Cart/Cart.php
@@ -49,13 +49,6 @@ class Cart
     protected $sessionKeyCartItems;
 
     /**
-     * the session key use to persist cart conditions
-     *
-     * @var
-     */
-    protected $sessionKeyCartConditions;
-
-    /**
      * Configuration to pass to ItemCollection
      *
      * @var
@@ -85,7 +78,6 @@ class Cart
         $this->instanceName = $instanceName;
         $this->sessionKey = $session_key;
         $this->sessionKeyCartItems = $this->sessionKey . '_cart_items';
-        $this->sessionKeyCartConditions = $this->sessionKey . '_cart_conditions';
         $this->config = $config;
         $this->currentItem = null;
         $this->fireEvent('created');
@@ -104,7 +96,6 @@ class Cart
 
         $this->sessionKey = $sessionKey;
         $this->sessionKeyCartItems = $this->sessionKey . '_cart_items';
-        $this->sessionKeyCartConditions = $this->sessionKey . '_cart_conditions';
 
         return $this;
     }
@@ -149,12 +140,11 @@ class Cart
      * @param float $price
      * @param int $quantity
      * @param array $attributes
-     * @param CartCondition|array $conditions
      * @param string $associatedModel
      * @return $this
      * @throws InvalidItemException
      */
-    public function add($id, $name = null, $price = null, $quantity = null, $attributes = array(), $conditions = array(), $associatedModel = null)
+    public function add($id, $name = null, $price_pln = null, $price_eur = null, $quantity = null, $attributes = array(), $associatedModel = null)
     {
         // if the first argument is an array,
         // we will need to call add again
@@ -166,10 +156,10 @@ class Cart
                     $this->add(
                         $item['id'],
                         $item['name'],
-                        $item['price'],
+                        $item['price_pln'],
+                        $item['price_eur'],
                         $item['quantity'],
                         Helpers::issetAndHasValueOrAssignDefault($item['attributes'], array()),
-                        Helpers::issetAndHasValueOrAssignDefault($item['conditions'], array()),
                         Helpers::issetAndHasValueOrAssignDefault($item['associatedModel'], null)
                     );
                 }
@@ -177,10 +167,10 @@ class Cart
                 $this->add(
                     $id['id'],
                     $id['name'],
-                    $id['price'],
+                    $id['price_pln'],
+                    $id['price_eur'],
                     $id['quantity'],
                     Helpers::issetAndHasValueOrAssignDefault($id['attributes'], array()),
-                    Helpers::issetAndHasValueOrAssignDefault($id['conditions'], array()),
                     Helpers::issetAndHasValueOrAssignDefault($id['associatedModel'], null)
                 );
             }
@@ -191,10 +181,10 @@ class Cart
         $data = array(
             'id' => $id,
             'name' => $name,
-            'price' => Helpers::normalizePrice($price),
+            'price_pln' => Helpers::normalizePrice($price_pln),
+            'price_eur' => Helpers::normalizePrice($price_eur),
             'quantity' => $quantity,
             'attributes' => new ItemAttributeCollection($attributes),
-            'conditions' => $conditions
         );
 
         if (isset($associatedModel) && $associatedModel != '') {
@@ -278,40 +268,6 @@ class Cart
     }
 
     /**
-     * add condition on an existing item on the cart
-     *
-     * @param int|string $productId
-     * @param CartCondition $itemCondition
-     * @return $this
-     */
-    public function addItemCondition($productId, $itemCondition)
-    {
-        if ($product = $this->get($productId)) {
-            $conditionInstance = "\\Darryldecode\\Cart\\CartCondition";
-
-            if ($itemCondition instanceof $conditionInstance) {
-                // we need to copy first to a temporary variable to hold the conditions
-                // to avoid hitting this error "Indirect modification of overloaded element of Darryldecode\Cart\ItemCollection has no effect"
-                // this is due to laravel Collection instance that implements Array Access
-                // // see link for more info: http://stackoverflow.com/questions/20053269/indirect-modification-of-overloaded-element-of-splfixedarray-has-no-effect
-                $itemConditionTempHolder = $product['conditions'];
-
-                if (is_array($itemConditionTempHolder)) {
-                    array_push($itemConditionTempHolder, $itemCondition);
-                } else {
-                    $itemConditionTempHolder = $itemCondition;
-                }
-
-                $this->update($productId, array(
-                    'conditions' => $itemConditionTempHolder // the newly updated conditions
-                ));
-            }
-        }
-
-        return $this;
-    }
-
-    /**
      * removes an item on cart by item ID
      *
      * @param $id
@@ -352,206 +308,6 @@ class Cart
         return true;
     }
 
-    /**
-     * add a condition on the cart
-     *
-     * @param CartCondition|array $condition
-     * @return $this
-     * @throws InvalidConditionException
-     */
-    public function condition($condition)
-    {
-        if (is_array($condition)) {
-            foreach ($condition as $c) {
-                $this->condition($c);
-            }
-
-            return $this;
-        }
-
-        if (!$condition instanceof CartCondition) throw new InvalidConditionException('Argument 1 must be an instance of \'Darryldecode\Cart\CartCondition\'');
-
-        $conditions = $this->getConditions();
-
-        // Check if order has been applied
-        if ($condition->getOrder() == 0) {
-            $last = $conditions->last();
-            $condition->setOrder(!is_null($last) ? $last->getOrder() + 1 : 1);
-        }
-
-        $conditions->put($condition->getName(), $condition);
-
-        $conditions = $conditions->sortBy(function ($condition, $key) {
-            return $condition->getOrder();
-        });
-
-        $this->saveConditions($conditions);
-
-        return $this;
-    }
-
-    /**
-     * get conditions applied on the cart
-     *
-     * @return CartConditionCollection
-     */
-    public function getConditions()
-    {
-        return new CartConditionCollection($this->session->get($this->sessionKeyCartConditions));
-    }
-
-    /**
-     * get condition applied on the cart by its name
-     *
-     * @param $conditionName
-     * @return CartCondition
-     */
-    public function getCondition($conditionName)
-    {
-        return $this->getConditions()->get($conditionName);
-    }
-
-    /**
-     * Get all the condition filtered by Type
-     * Please Note that this will only return condition added on cart bases, not those conditions added
-     * specifically on an per item bases
-     *
-     * @param $type
-     * @return CartConditionCollection
-     */
-    public function getConditionsByType($type)
-    {
-        return $this->getConditions()->filter(function (CartCondition $condition) use ($type) {
-            return $condition->getType() == $type;
-        });
-    }
-
-
-    /**
-     * Remove all the condition with the $type specified
-     * Please Note that this will only remove condition added on cart bases, not those conditions added
-     * specifically on an per item bases
-     *
-     * @param $type
-     * @return $this
-     */
-    public function removeConditionsByType($type)
-    {
-        $this->getConditionsByType($type)->each(function ($condition) {
-            $this->removeCartCondition($condition->getName());
-        });
-    }
-
-
-    /**
-     * removes a condition on a cart by condition name,
-     * this can only remove conditions that are added on cart bases not conditions that are added on an item/product.
-     * If you wish to remove a condition that has been added for a specific item/product, you may
-     * use the removeItemCondition(itemId, conditionName) method instead.
-     *
-     * @param $conditionName
-     * @return void
-     */
-    public function removeCartCondition($conditionName)
-    {
-        $conditions = $this->getConditions();
-
-        $conditions->pull($conditionName);
-
-        $this->saveConditions($conditions);
-    }
-
-    /**
-     * remove a condition that has been applied on an item that is already on the cart
-     *
-     * @param $itemId
-     * @param $conditionName
-     * @return bool
-     */
-    public function removeItemCondition($itemId, $conditionName)
-    {
-        if (!$item = $this->getContent()->get($itemId)) {
-            return false;
-        }
-
-        if ($this->itemHasConditions($item)) {
-            // NOTE:
-            // we do it this way, we get first conditions and store
-            // it in a temp variable $originalConditions, then we will modify the array there
-            // and after modification we will store it again on $item['conditions']
-            // This is because of ArrayAccess implementation
-            // see link for more info: http://stackoverflow.com/questions/20053269/indirect-modification-of-overloaded-element-of-splfixedarray-has-no-effect
-
-            $tempConditionsHolder = $item['conditions'];
-
-            // if the item's conditions is in array format
-            // we will iterate through all of it and check if the name matches
-            // to the given name the user wants to remove, if so, remove it
-            if (is_array($tempConditionsHolder)) {
-                foreach ($tempConditionsHolder as $k => $condition) {
-                    if ($condition->getName() == $conditionName) {
-                        unset($tempConditionsHolder[$k]);
-                    }
-                }
-
-                $item['conditions'] = $tempConditionsHolder;
-            }
-
-            // if the item condition is not an array, we will check if it is
-            // an instance of a Condition, if so, we will check if the name matches
-            // on the given condition name the user wants to remove, if so,
-            // lets just make $item['conditions'] an empty array as there's just 1 condition on it anyway
-            else {
-                $conditionInstance = "Darryldecode\\Cart\\CartCondition";
-
-                if ($item['conditions'] instanceof $conditionInstance) {
-                    if ($tempConditionsHolder->getName() == $conditionName) {
-                        $item['conditions'] = array();
-                    }
-                }
-            }
-        }
-
-        $this->update($itemId, array(
-            'conditions' => $item['conditions']
-        ));
-
-        return true;
-    }
-
-    /**
-     * remove all conditions that has been applied on an item that is already on the cart
-     *
-     * @param $itemId
-     * @return bool
-     */
-    public function clearItemConditions($itemId)
-    {
-        if (!$item = $this->getContent()->get($itemId)) {
-            return false;
-        }
-
-        $this->update($itemId, array(
-            'conditions' => array()
-        ));
-
-        return true;
-    }
-
-    /**
-     * clears all conditions on a cart,
-     * this does not remove conditions that has been added specifically to an item/product.
-     * If you wish to remove a specific condition to a product, you may use the method: removeItemCondition($itemId, $conditionName)
-     *
-     * @return void
-     */
-    public function clearCartConditions()
-    {
-        $this->session->put(
-            $this->sessionKeyCartConditions,
-            array()
-        );
-    }
 
     /**
      * get cart sub total without conditions
@@ -579,36 +335,11 @@ class Cart
         $cart = $this->getContent();
 
         $sum = $cart->sum(function (ItemCollection $item) {
-            return $item->getPriceSumWithConditions(false);
+            return $item->getPriceSum();
         });
 
-        // get the conditions that are meant to be applied
-        // on the subtotal and apply it here before returning the subtotal
-        $conditions = $this
-            ->getConditions()
-            ->filter(function (CartCondition $cond) {
-                return $cond->getTarget() === 'subtotal';
-            });
+        return Helpers::formatValue(floatval($sum), $formatted, $this->config);
 
-        // if there is no conditions, lets just return the sum
-        if (!$conditions->count()) return Helpers::formatValue(floatval($sum), $formatted, $this->config);
-
-        // there are conditions, lets apply it
-        $newTotal = 0.00;
-        $process = 0;
-
-        $conditions->each(function (CartCondition $cond) use ($sum, &$newTotal, &$process) {
-
-            // if this is the first iteration, the toBeCalculated
-            // should be the sum as initial point of value.
-            $toBeCalculated = ($process > 0) ? $newTotal : $sum;
-
-            $newTotal = $cond->applyCondition($toBeCalculated);
-
-            $process++;
-        });
-
-        return Helpers::formatValue(floatval($newTotal), $formatted, $this->config);
     }
 
     /**
@@ -624,27 +355,8 @@ class Cart
 
         $process = 0;
 
-        $conditions = $this
-            ->getConditions()
-            ->filter(function (CartCondition $cond) {
-                return $cond->getTarget() === 'total';
-            });
+        return Helpers::formatValue($subTotal, $this->config['format_numbers'], $this->config);
 
-        // if no conditions were added, just return the sub total
-        if (!$conditions->count()) {
-            return Helpers::formatValue($subTotal, $this->config['format_numbers'], $this->config);
-        }
-
-        $conditions
-            ->each(function (CartCondition $cond) use ($subTotal, &$newTotal, &$process) {
-                $toBeCalculated = ($process > 0) ? $newTotal : $subTotal;
-
-                $newTotal = $cond->applyCondition($toBeCalculated);
-
-                $process++;
-            });
-
-        return Helpers::formatValue($newTotal, $this->config['format_numbers'], $this->config);
     }
 
     /**
@@ -698,7 +410,8 @@ class Cart
     {
         $rules = array(
             'id' => 'required',
-            'price' => 'required|numeric',
+            'price_pln' => 'required|numeric',
+            'price_eur' => 'required|numeric',
             'quantity' => 'required|numeric|min:0.1',
             'name' => 'required',
         );
@@ -744,37 +457,6 @@ class Cart
     protected function save($cart)
     {
         $this->session->put($this->sessionKeyCartItems, $cart);
-    }
-
-    /**
-     * save the cart conditions
-     *
-     * @param $conditions
-     */
-    protected function saveConditions($conditions)
-    {
-        $this->session->put($this->sessionKeyCartConditions, $conditions);
-    }
-
-    /**
-     * check if an item has condition
-     *
-     * @param $item
-     * @return bool
-     */
-    protected function itemHasConditions($item)
-    {
-        if (!isset($item['conditions'])) return false;
-
-        if (is_array($item['conditions'])) {
-            return count($item['conditions']) > 0;
-        }
-
-        $conditionInstance = "Darryldecode\\Cart\\CartCondition";
-
-        if ($item['conditions'] instanceof $conditionInstance) return true;
-
-        return false;
     }
 
     /**
--- src/Darryldecode/Cart/ItemCollection.php
+++ src/Darryldecode/Cart/ItemCollection.php
@@ -7,8 +7,9 @@
  * Time: 11:03 AM
  */
 
-use Darryldecode\Cart\Helpers\Helpers;
 use Illuminate\Support\Collection;
+use Darryldecode\Cart\Helpers\Helpers;
+use Illuminate\Support\Facades\Config;
 
 class ItemCollection extends Collection
 {
@@ -39,7 +40,14 @@ class ItemCollection extends Collection
      */
     public function getPriceSum()
     {
-        return Helpers::formatValue($this->price * $this->quantity, $this->config['format_numbers'], $this->config);
+        if(Config::get('app.locale') == "pl")
+        {
+            $price = $this->price_pln;
+        }
+        else{
+            $price = $this->price_eur;
+        }
+        return Helpers::formatValue($price * $this->quantity, $this->config['format_numbers'], $this->config);
     }
 
     public function __get($name)
@@ -48,86 +56,5 @@ class ItemCollection extends Collection
             return !is_null($this->get($name)) ? $this->get($name) : $this->getAssociatedModel();
         }
         return null;
-    }
-
-    /**
-     * return the associated model of an item
-     *
-     * @return bool
-     */
-    protected function getAssociatedModel()
-    {
-        if (!$this->has('associatedModel')) {
-            return null;
-        }
-
-        $associatedModel = $this->get('associatedModel');
-
-        return with(new $associatedModel())->find($this->get('id'));
-    }
-
-    /**
-     * check if item has conditions
-     *
-     * @return bool
-     */
-    public function hasConditions()
-    {
-        if (!isset($this['conditions'])) return false;
-        if (is_array($this['conditions'])) {
-            return count($this['conditions']) > 0;
-        }
-        $conditionInstance = "Darryldecode\\Cart\\CartCondition";
-        if ($this['conditions'] instanceof $conditionInstance) return true;
-
-        return false;
-    }
-
-    /**
-     * check if item has conditions
-     *
-     * @return mixed|null
-     */
-    public function getConditions()
-    {
-        if (!$this->hasConditions()) return [];
-        return $this['conditions'];
-    }
-
-    /**
-     * get the single price in which conditions are already applied
-     * @param bool $formatted
-     * @return mixed|null
-     */
-    public function getPriceWithConditions($formatted = true)
-    {
-        $originalPrice = $this->price;
-        $newPrice = 0.00;
-        $processed = 0;
-
-        if ($this->hasConditions()) {
-            if (is_array($this->conditions)) {
-                foreach ($this->conditions as $condition) {
-                    ($processed > 0) ? $toBeCalculated = $newPrice : $toBeCalculated = $originalPrice;
-                    $newPrice = $condition->applyCondition($toBeCalculated);
-                    $processed++;
-                }
-            } else {
-                $newPrice = $this['conditions']->applyCondition($originalPrice);
-            }
-
-            return Helpers::formatValue($newPrice, $formatted, $this->config);
-        }
-        return Helpers::formatValue($originalPrice, $formatted, $this->config);
-    }
-
-    /**
-     * get the sum of price in which conditions are already applied
-     * @param bool $formatted
-     * @return mixed|null
-     */
-    public function getPriceSumWithConditions($formatted = true)
-    {
-        return Helpers::formatValue($this->getPriceWithConditions(false) * $this->quantity, $formatted, $this->config);
     }
 }
